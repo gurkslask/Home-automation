e6dfd8681e347b5336a468c3fc95a4469db255e8
Alexander Svensson <gurkslask@gmail.com>
2013-10-15 20:58:53 +0200

    Ändrat ds filen

--- ds1820.py
+++ ds1820.py
@@ -1,6 +1,13 @@
+import time
+def Read_temp():
+	with open('/sys/bus/w1/devices/28-00000523a1cb/w1_slave', 'r') as tempfile:
+		data = tempfile.read()
+		print(data.find('t='))
+		temp_pos = data.find('t=')
+		print(data[temp_pos+2:])
 
-with open('/sys/bus/w1/devices/28-00000523a1cb/w1_slave', 'r') as tempfile:
-	data = tempfile.read()
-	print(data.find('t='))
-	temp_pos = data.find('t=')
-	print(data[temp_pos:])
+def Write_temp(in)
+	with open('test') as infile:
+		data = infile.read()
+	with open('test', 'w') as outfile:
+        a_file.write(data + '\n' + in)
