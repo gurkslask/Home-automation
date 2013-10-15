362fac5f561552d4d482cb72324b2c562d37c65f
Alexander Svensson <gurkslask@gmail.com>
2013-10-15 20:37:50 +0200

    asdS

--- ds1820.py
+++ ds1820.py
@@ -1,6 +1,7 @@
 
 with open('/sys/bus/w1/devices/28-00000523a1cb/w1_slave', 'r') as tempfile:
     data = sum(1 for line in tempfile)
+    print(data)
     #print tempfile.readline(10)
     for x in range(0,data):
         print(tempfile.readline(x))
