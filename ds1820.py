97c51dcf57a1efad0b130b8044cfb687ed07e97f
Alexander Svensson <gurkslask@gmail.com>
2013-10-16 08:22:39 +0200

    Ändrat i ds filen

--- ds1820.py
+++ ds1820.py
@@ -1,13 +1,26 @@
+import datetime
 import time
-def Read_temp():
-	with open('/sys/bus/w1/devices/28-00000523a1cb/w1_slave', 'r') as tempfile:
-		data = tempfile.read()
-		print(data.find('t='))
-		temp_pos = data.find('t=')
-		print(data[temp_pos+2:])
 
-def Write_temp(in)
-	with open('test') as infile:
-		data = infile.read()
-	with open('test', 'w') as outfile:
-        a_file.write(data + '\n' + in)
+
+timestamp_minute = time.time()
+
+def Read_temp():
+        with open('/sys/bus/w1/devices/28-00000523a1cb/w1_slave', 'r') as tempfile:
+                data = tempfile.read()
+                print(data.find('t='))
+                temp_pos = data.find('t=')
+                print(data[temp_pos+2:])
+
+def Write_temp(indata):
+        with open(r'C:/Projects/Home-automation/test.txt', 'a') as outfile:
+                outfile.write(indata)
+
+while True:
+	if time.time() - 60 > timestamp_minute :
+		now = datetime.datetime.now()
+		stringen = str(now.day) + str(now.month) + str(now.minute) + ' - 5555\n'
+		print( stringen)
+		Write_temp( stringen)
+		timestamp_minute = time.time()
+
+
