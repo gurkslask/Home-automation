a99bcfdea0973be7569fde1e9ab2a5dfdcd3efb6
Alexander Svensson <gurkslask@gmail.com>
2013-10-21 20:07:35 +0200

    Some changes

--- ds1820class.py
+++ ds1820class.py
@@ -1,9 +1,12 @@
+import datetime
+import time
 class DS1820(adress):
 	'''Class for working with a DS1820 temperature sensor
 	'''
 	def __init__(self):
 		'''Some initialization		'''
 		self.adress = adress
+		self.temp = 0.0
 
 	def ReadTemp(self):
 		'''Read the file that stores temperature data, use the folder
@@ -13,4 +16,18 @@ class DS1820(adress):
             temp_pos = data.find('t=') #Look for t, this is where the temperature is
             data = data[temp_pos+2:]#remove 't='
             data = int(data)/1000#insert comma, 
+            self.temp = data
             return data
+
+    def Write_temp(self, interval):
+    	'''interval in seconds, dont update until interval is reached
+    	Dont really know how the time stamp should work'''
+    	timestamp = time.time()
+    	if time.time() - interval > timestamp:
+    		now = datetime.datetime.now()
+    		if self.data_string == self.temp:*'''If temp is same as before, just insert a _'''
+    			self.data_string = '_'
+    		else:
+    			self.data_string = self.temp
+	        with open('sensors/'now.year + now.month + now.day + '/' + self.adress, 'a') as outfile:
+	                outfile.write(self.data_string)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ds1820class import DS1820
+import time
 
 GT1 = DS1820('28-00000523a1cb')
 print(GT1.ReadTemp())
