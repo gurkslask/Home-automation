a7a65468d92b791b3890d72c2477c71ae96b9543
Gurkslask <Gurkslask@gmail.com>
2014-01-05 13:41:10 +0100

    Added a menu to main

--- main.py
+++ main.py
@@ -4,64 +4,121 @@ from Kompensering import Kompensering
 from OpenCloseValveClass import OpenCloseValve
 from IOdef import IOdef
 import time
+import threading
 
-#Declare IO Variables
-IOVariables = IOdef()
 
-#Declare temperaturecompensation
-Komp = Kompensering()
-Komp.SetVarden(20, 17)
-Komp.SetVarden(-10, 40)
-Komp.SetVarden(0, 35)
-Komp.SetVarden(10, 30)
-Komp.SetVarden(-20, 65)
-Komp.SetMax(65)
-Komp.SetMin(20)
 
-#Loggin of the compensation
-Setpoint_VS1 = 0.0
-Setpoint_Log_VS1 = Write_temp(Setpoint_VS1,'VS1_Setpoint')
+class MainLoop():
+	def __init__(self):
 
-#Declare temperature sensors
-#Framledning
-GT1 = DS1820('28-00000523a1cb')
-#Retur
-VS1_GT2 = DS1820('28-00000524056e')
-#Ute
-VS1_GT3 = DS1820('28-0000052407e0')
+		#Declare IO Variables
+		self.IOVariables = IOdef()
 
-#Declare logging interval
-GT1.SetWriteInterval(60)
-VS1_GT2.SetWriteInterval(60)
-VS1_GT3.SetWriteInterval(60)
+		#Declare temperaturecompensation
+		self.Komp = Kompensering()
+		self.Komp.SetVarden(20, 17)
+		self.Komp.SetVarden(-10, 40)
+		self.Komp.SetVarden(0, 35)
+		self.Komp.SetVarden(10, 30)
+		self.Komp.SetVarden(-20, 65)
+		self.Komp.SetMax(65)
+		self.Komp.SetMin(20)
 
-#Declare Heating valve
-VS1_SV1_Class = OpenCloseValve()
+		#Loggin of the compensation
+		self.Setpoint_VS1 = 0.0
+		self.Setpoint_Log_VS1 = Write_temp(Setpoint_VS1,'VS1_Setpoint')
 
-ActTimeLoop1 = time.time()
-ActTimeLoop2 = time.time()
+		#Declare temperature sensors
+		#Framledning
+		self.GT1 = DS1820('28-00000523a1cb')
+		#Retur
+		self.VS1_GT2 = DS1820('28-00000524056e')
+		#Ute
+		self.VS1_GT3 = DS1820('28-0000052407e0')
 
-while True:
-	'''This is the main loop'''
-	if ActTimeLoop1 +20< time.time():
-		ActTimeLoop1 = time.time()
+		#Declare logging interval
+		self.GT1.SetWriteInterval(60)
+		self.VS1_GT2.SetWriteInterval(60)
+		self.VS1_GT3.SetWriteInterval(60)
 
+		#Declare Heating valve
+		self.VS1_SV1_Class = OpenCloseValve()
+
+		#Initialize the loops
+		self.ActTimeLoop1 = time.time()
+		self.ActTimeLoop2 = time.time()
+
+		#Interaction menu
+		self.choices = {
+					"1" : self.ChangeSP, 
+					"2" : self.ShowValues
+				}
+
+	def ControlLoop(self):
+			while True:
+		'''This is the main loop'''
+		if ActTimeLoop1 +20< time.time():
+			ActTimeLoop1 = time.time()
+
+			#print('GT1 {0:.1f}'.format(GT1.RunMainTemp()))
+			#print('GT2 {0:.1f}'.format(VS1_GT2.RunMainTemp()))
+			#print('GT3 {0:.1f}'.format(VS1_GT3.RunMainTemp()))
+			
+			Setpoint_VS1 = Komp.CountSP(VS1_GT3.temp)
+			Setpoint_Log_VS1.value = Setpoint_VS1
+			#print('SP {0:.1f}'.format(Setpoint_VS1))
+			Setpoint_Log_VS1.main()
+
+			#print('Loop 1')
+
+		if ActTimeLoop2 +5< time.time():
+			ActTimeLoop2 = time.time()
+			VS1_SV1_Class.main(GT1.temp , Setpoint_VS1)
+			IOVariables['b_SV_CLOSE_DO']['Value'] = VS1_SV1_Class.Man_Close
+			IOVariables['b_SV_OPEN_DO']['Value'] = VS1_SV1_Class.Man_Open
+			#print('Loop 2')
+
+		time.sleep(4)
+
+
+
+	def InteractionLoop(self):
+		while True:
+			self.DisplayMenu()
+			print("""Home-automation menu:
+				1. Change Setpoint
+				2. Show values
+				""")
+			choice=input('Enter an option: ')
+			self.choices.get(choice)
+			if action:
+				action()
+			else:
+				print("{0} is not a valid choice".format(choice))
+
+	def ChangeSP(self):
+		value1 = input('Enter outside temperature: ')
+		value2 = input('Enter forward temperature: ')
+		self.Setpoint_VS1.DictVarden[value1] = value2
+
+	def ShowValues(self):
 		print('GT1 {0:.1f}'.format(GT1.RunMainTemp()))
 		print('GT2 {0:.1f}'.format(VS1_GT2.RunMainTemp()))
 		print('GT3 {0:.1f}'.format(VS1_GT3.RunMainTemp()))
-		
-		Setpoint_VS1 = Komp.CountSP(VS1_GT3.temp)
-		Setpoint_Log_VS1.value = Setpoint_VS1
 		print('SP {0:.1f}'.format(Setpoint_VS1))
-		Setpoint_Log_VS1.main()
 
-		print('Loop 1')
 
-	if ActTimeLoop2 +5< time.time():
-		ActTimeLoop2 = time.time()
-		VS1_SV1_Class.main(GT1.temp , Setpoint_VS1)
-		IOVariables['b_SV_CLOSE_DO']['Value'] = VS1_SV1_Class.Man_Close
-		IOVariables['b_SV_OPEN_DO']['Value'] = VS1_SV1_Class.Man_Open
-		print('Loop 2')
 
-	time.sleep(4)
+
+def main():
+	ML = MainLoop()
+	threading.Thread(target=ML.ControlLoop).start()
+	threading.Thread(target=ML.InteractionLoop).start()
+
+
+if __name__ == '__main__':
+	main()
+
+
+
+
