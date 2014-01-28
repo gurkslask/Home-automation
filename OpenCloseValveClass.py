ed839d7af4aefa83739ee0b30271e93f45de7916
Alexander Svensson <gurkslask@gmail.com>
2014-01-28 20:35:36 +0000

    This is running

--- OpenCloseValveClass.py
+++ OpenCloseValveClass.py
@@ -9,6 +9,8 @@ class OpenCloseValve(object):
 		self.Time_Close = 0.5 #Seconds the valve shall close
 		self.Man_Open = False
 		self.Man_Close = False
+		self.Man_Close_OUT = False
+		self.Man_Open_OUT = False
 		self.Write_Stat_Open = Write_temp(self.Man_Open, 'VS1_SV1_Open')
 		self.Write_Stat_Close = Write_temp(self.Man_Close, 'VS1_SV1_Close')
 		self.Control_Time = 0
@@ -38,14 +40,14 @@ class OpenCloseValve(object):
 			self.Control_Time = time.time() 
 			self.Control_Active = True
 		if self.Man_Close:
-			if self.Control_Time + Time_Close < time.time():
+			if self.Control_Time + self.Time_Close < time.time():
 				self.Man_Close_OUT = True
 				#Close variable
 			else:
 				self.Man_Close_OUT = False
 				self.Control_Active = False
 		if self.Man_Open:
-			if self.Control_Time + Time_Open < time.time():
+			if self.Control_Time + self.Time_Open < time.time():
 				self.Man_Open_OUT = True
 				#Open variable
 			else:
