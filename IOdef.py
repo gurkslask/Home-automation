41cbaf15e994cacb8ae4a6dbe0960109f674465d
Gurkslask <Gurkslask@gmail.com>
2014-05-19 20:07:55 +0200

    added pumpcontrol and some webscraping

--- IOdef.py
+++ IOdef.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 def IOdef():
 	IOVariables={
-	'b_P1_DO': {'Value': 0, 'IOdevice': 2, 'IOadress': 1},
-	'b_SV_OPEN_DO': {'Value': 0, 'IOdevice': 2, 'IOadress': 2},
-	'b_SV_CLOSE_DO': {'Value': 0, 'IOdevice': 2, 'IOadress': 3},
-
+	'b_P1_DO': {'Value': 0, 'IOdevice': 2, 'IOadress': 1, 'Comment': 'Radiator cirk pumpen'},
+	'b_SV_OPEN_DO': {'Value': 0, 'IOdevice': 2, 'IOadress': 2, 'Comment': 'Open heating valve'}, 
+	'b_SV_CLOSE_DO': {'Value': 0, 'IOdevice': 2, 'IOadress': 3, 'Comment': 'Close heating valve'},
+	'b_P2_DO': {'Value': 0, 'IOdevice': 2, 'IOadress': 4, 'Comment': 'Sunwarming cirk pump'},
 	}
 	return IOVariables
 	
