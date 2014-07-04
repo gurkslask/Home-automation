8fcdba64b999703767e7708be6f5b11ff13e9bae
Gurkslask <Gurkslask@gmail.com>
2014-07-04 23:02:08 +0200

    minor change

--- IOdef.py
+++ IOdef.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 def IOdef():
 	IOVariables={
-	'b_P1_DO': {'Value': 0, 'IOdevice': 2, 'IOadress': 1, 'Comment': 'Radiator cirk pumpen'},
-	'b_SV_OPEN_DO': {'Value': 0, 'IOdevice': 2, 'IOadress': 2, 'Comment': 'Open heating valve'}, 
-	'b_SV_CLOSE_DO': {'Value': 0, 'IOdevice': 2, 'IOadress': 3, 'Comment': 'Close heating valve'},
-	'b_P2_DO': {'Value': 0, 'IOdevice': 2, 'IOadress': 4, 'Comment': 'Sunwarming cirk pump'},
-	'b_Test': {'Value': 0, 'IOdevice': 2, 'IOadress': 5, 'Comment': 'Test var'},
+	'b_P1_DO': {'Value': 0, 'IOdevice': 2048, 'IOadress': 1, 'Comment': 'Radiator cirk pumpen'},
+	'b_SV_OPEN_DO': {'Value': 0, 'IOdevice': 2048, 'IOadress': 2, 'Comment': 'Open heating valve'}, 
+	'b_SV_CLOSE_DO': {'Value': 0, 'IOdevice': 2048, 'IOadress': 3, 'Comment': 'Close heating valve'},
+	'b_P2_DO': {'Value': 0, 'IOdevice': 2048, 'IOadress': 4, 'Comment': 'Sunwarming cirk pump'},
+	'b_Test': {'Value': 0, 'IOdevice': 2048, 'IOadress': 5, 'Comment': 'Test var'},
 	}
 	return IOVariables
 	
