ff2a25c6251a50c15db836f76daa98cbd29fd297
Alexander Svensson <gurkslask@gmail.com>
2014-07-13 11:05:35 +0000

    added 96 hour view to trends

--- Flask/Flask.py
+++ Flask/Flask.py
@@ -58,6 +58,55 @@ def simple():
 	response.headers['Content-Type'] = 'image/png'
 	return response
 
+
+
+
+@app.route("/simple96.png")
+def simple96():
+	import datetime as dt
+	import StringIO
+	import random
+	import os
+	import time
+ 
+	from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+	from matplotlib.figure import Figure
+	from matplotlib.dates import DateFormatter
+ 
+	fig=Figure(figsize=(12,10))
+	ax=fig.add_subplot(111)
+	To = time.time()
+	From = To - 345600
+	os.chdir('/home/pi/Projects/Home-automation/sensors')
+	for i in os.listdir(os.getcwd()):
+		dates=[]
+		values=[]
+		data_dict={}
+		os.chdir(i)
+		file_list=[l for l in os.listdir(os.getcwd()) if int(To)+345600  > int(l) > int(From)-345600]
+		#print(file_list)
+		for j in file_list:
+			with open(j, 'r') as f:
+				for k in f:
+					split_list = k.split('|')
+					if To > int(split_list[0]) > From:
+						data_dict[split_list[0]]=split_list[1]
+
+		for key in sorted(data_dict.keys()):
+			dates.append(dt.datetime.fromtimestamp(int(key)))
+			values.append(data_dict[key])
+		ax.plot_date(dates, values, '-', label='aaa')
+		os.chdir('..')
+	ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+	fig.autofmt_xdate()
+	#fig.legend(loc='upper left')
+	canvas=FigureCanvas(fig)
+	png_output = StringIO.StringIO()
+	canvas.print_png(png_output)
+	response=make_response(png_output.getvalue())
+	response.headers['Content-Type'] = 'image/png'
+	return response	
+
 @app.route('/shutdown', methods=['POST'])
 def shutdown():
 	shutdown_server()
