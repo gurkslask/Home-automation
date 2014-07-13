fcae4adb7e54fb4efdfc2c607593da8e2b85bda9
Alexander Svensson <gurkslask@gmail.com>
2014-07-13 08:32:08 +0000

    small bug

--- PumpControl.py
+++ PumpControl.py
@@ -37,7 +37,7 @@ def Control_of_CP2(Weather, Out_temperature, Tank_temperature, Sun_heater_temper
 		hysteresis = Weather_dict[Weather]
 	except:
 		hysteresis = 7
-		print('Bad weather string!{1}'.format(1=Weather))
+		print('Bad weather string!{w}'.format(w=Weather))
 	IOoutput=False		
 	if Sun_heater_temperature > Tank_temperature +  hysteresis:
 		IOoutput = True
