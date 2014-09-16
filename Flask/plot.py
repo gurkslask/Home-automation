32330c565a74c5497664308df046bc348bc834a7
Gurkslask <Gurkslask@gmail.com>
2014-09-16 22:07:36 +0200

    Beginning to work on a plot class

---
+++ Flask/plot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/en
+
+
+class PlotClass(object):
+    def __init__(self):
+        pass
+
+    def plot(self):
+        print('here i am')
+
+
+if __name__ == '__main__':
+    p = PlotClass()
+    p.plot()
