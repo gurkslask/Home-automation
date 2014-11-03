b7377b8da6a0b5ad6f7210733d7ec3464a0e0ca9
Alexander Svensson <gurkslask@gmail.com>
2014-11-03 19:06:52 +0000

    heh

--- Flask/bokeh_plot.py
+++ Flask/bokeh_plot.py
@@ -1,18 +1,37 @@
-import bokeh as bk
-import pandas as pd
+#import bokeh as bk
+#import pandas as pd
 import sqlite3 as lite
 import datetime as dt
 
+
 def LoadFromSQL():
-    conn = lite.connect('~/Projects/Home-automation/data.db')
+    conn = lite.connect('/home/pi/Projects/Home-automation/data.db')
     cur = conn.cursor()
     with conn:
-        cur.execute('select * from VS1_GT3  between {} and {}'.format(
-            dt.datetime.now(), dt.timedelta(hours=-3)))
+        print('select * from VS1_GT3 where Time between {} and {}'.format(
+            dt.datetime.now(),
+            (dt.datetime.now() - dt.timedelta(days=3))
+            ))
+
+        cur.execute("""
+            select *
+            from VS1_GT3
+            where Time between "{}" and "{}"
+            """
+                    .format(
+                        dt.datetime.now(),
+                        dt.datetime.now() - dt.timedelta(days=10)
+                        ))
+
         data = cur.fetchall()
-    pd_data = pd.DataFrame(date, columns=['Time, Temp'])
-    print(pd_data)
+    print(len(data))
+    #pd_data = pd.DataFrame(data, columns=['Time, Temp'])
+    #print(pd_data)
+    return data
 
 
 def PlotBokeh():
     pass
+
+if __name__ == '__main__':
+    LoadFromSQL()
